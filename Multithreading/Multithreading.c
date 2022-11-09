b94e315d14dcad8ea3b1d91630a513a5792b8b2b
suroshe2013 <suroshe2013@gmail.com>
2022-11-09 12:52:18 +0530

    readme update

--- Multithreading/Multithreading.c
+++ Multithreading/Multithreading.c
@@ -1,4 +1,8 @@
-/*Write a multithreaded program that calculates various statistical values for a list of numbers. This program will be passed a series of numbers on the command line and will then create three separate worker threads. One thread will determine the average of the numbers, the second will determine the maximum value, and the third will determine the minimum value. For example, suppose your program is passed the integers 90 81 78 95 79 72 85 The program will report the average value to be 82, the minimum value to be 72 and the maximum value to be 95 The variables representing the average, minimum, and maximum values will be stored globally. The worker threads will set these values, and the parent thread will output the values once the workers have exited. (We could obviously expand this program by creating additional threads that determine other statistical values, such as median and standard deviation.)*/
+/*Write a multithreaded program that calculates various statistical values for a list of numbers. This program will be passed a series of numbers on the command line and will then create three separate worker threads.
+One thread will determine the average of the numbers, the second will determine the maximum value, and the third will determine the minimum value.
+For example, suppose your program is passed the integers 90 81 78 95 79 72 85 The program will report the average value to be 82, the minimum value to be 72 and the maximum value to be 95.
+The variables representing the average, minimum, and maximum values will be stored globally. The worker threads will set these values, and the parent thread will output the values once the workers have exited. 
+(We could obviously expand this program by creating additional threads that determine other statistical values, such as median and standard deviation.)*/
 #include<stdio.h>
 #include<pthread.h>
 
